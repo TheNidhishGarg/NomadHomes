47469df93470a2980f575829ae538149c6328ae7
The Nidhish Garg <nidhishgarg021@gmail.com>
2026-05-07 16:15:16 +0530

    Form validations fixed for adding home

--- schema.js
+++ schema.js
@@ -6,7 +6,6 @@ module.exports.listingSchema = Joi.object({
   location: Joi.string().required(),
   country: Joi.string().required(),
   price: Joi.number().required().min(0),
-  imageUrl: Joi.string().allow("", null),
 });
 
 module.exports.reviewSchema = Joi.object({
